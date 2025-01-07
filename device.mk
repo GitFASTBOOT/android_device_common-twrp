a786390a49340b921db44863fc7887c01405ab77
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-01-08 01:01:41 +0200

    set AB config

--- device.mk
+++ device.mk
@@ -32,3 +32,26 @@ endif
 
 # dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+
+ifeq ($(ENABLE_VIRTUAL_AB),true)
+# A/B
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    checkpoint_gc
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+endif
+
